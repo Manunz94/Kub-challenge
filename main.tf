2dce4c404f688d73d278ff179fe54e475a4d0d16
Manunz94 <manunz94@hotmail.it>
2024-06-18 17:12:46 +0200

    aggiornamento terraform

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-central-1"
 }
 resource "aws_instance" "manager" {
-  ami           = "ami-01e444924a2233b07" # Ubuntu 20.04 LTS - HVM
+  ami           = "ami-01e444924a2233b07" 
   instance_type = "t2.medium"
   count         = 1
   tags = {
@@ -10,10 +10,18 @@ resource "aws_instance" "manager" {
   }
 }
 resource "aws_instance" "worker" {
-  ami           = "ami-01e444924a2233b07" # Ubuntu 20.04 LTS - HVM
+  ami           = "ami-01e444924a2233b07" 
   instance_type = "t2.medium"
   count         = 2
   tags = {
-    Name = "k8s-worker"
+    Name = "k8s-worker-${count.index}"
   }
+}
+
+output "manager_public_ip" {
+  value = aws_instance.manager.public_ip
+}
+
+output "worker_public_ips" {
+  value = [for instance in aws_instance.worker : instance.public_ip]
 }
