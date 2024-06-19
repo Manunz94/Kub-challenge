76445593f34c689ad626bc1be98a39479724ddfd
Manunz94 <manunz94@hotmail.it>
2024-06-19 22:04:21 +0200

    cambio vpc e subnet

--- main.tf
+++ main.tf
@@ -13,8 +13,16 @@ resource "aws_security_group" "ssh_access" {
     from_port   = 22
     to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Apri SSH da qualsiasi indirizzo IP
+    cidr_blocks = ["0.0.0.0/0"]  
   }
+}
+
+variable "vpc_id" {
+  default = "vpc-0de3a6f018ddfa754"
+}
+
+variable "subnet_id_public" {
+  default = "subnet-06160d9e5f0521aa0"
 }
 
 resource "aws_instance" "manager" {
@@ -22,7 +30,8 @@ resource "aws_instance" "manager" {
   instance_type = "t2.medium"
   count         = 1
   key_name      = data.aws_key_pair.existing.key_name
-  security_groups = [aws_security_group.ssh_access.name]
+  #security_groups = [aws_security_group.ssh_access.name]
+  subnet_id     = var.subnet_id_public
   tags = {
     Name = "k8s-manager"
   }
@@ -33,7 +42,8 @@ resource "aws_instance" "worker" {
   instance_type = "t2.medium"
   count         = 2
   key_name      = data.aws_key_pair.existing.key_name
-  security_groups = [aws_security_group.ssh_access.name]
+  #security_groups = [aws_security_group.ssh_access.name]
+  subnet_id     = var.subnet_id_public
   tags = {
     Name = "k8s-worker"
   }
